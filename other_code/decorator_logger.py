eaed9a49de3da85f7c50c492d8d540e8665e5720
griseeminence <p.kolt080@gmail.com>
2023-10-22 21:13:26 +0800

    add decorators

---
+++ other_code/decorator_logger.py
@@ -0,0 +1,53 @@
+import sys
+import time
+import logging
+from logging.handlers import RotatingFileHandler
+from typing import Any, Callable
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+handler = RotatingFileHandler(
+    'logger.log',
+    maxBytes=50000000,
+    backupCount=5,
+    encoding='utf-8'
+)
+stream_handler = logging.StreamHandler(sys.stdout)
+logger.addHandler(stream_handler)
+logger.addHandler(handler)
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+handler.setFormatter(formatter)
+stream_handler.setFormatter(formatter)
+
+
+def logger_decorator(func: Callable[..., Any]) -> Callable[..., Any]:
+    """
+    Decorator for logging both in the file 'logger.log' and in the console.
+    """
+    def wrapper(*args, **kwargs):
+        try:
+            logger.debug('Starting function')
+            result = func(*args, **kwargs)
+            logger.debug('Ending function')
+        except Exception as error:
+            logger.error(f'{error}')
+            return f'An error occured: {error}'
+        return result
+
+    return wrapper
+
+
+@logger_decorator
+def counter(count: int) -> dict:
+    """
+    Dictionary with multiplication tables.
+    """
+    time.sleep(1)
+    return {i: i ** 2 for i in range(count)}
+
+
+if __name__ == '__main__':
+    size: int = int(input())
+    counter(size)
---
+++ other_code/decorator_timer.py
@@ -0,0 +1,33 @@
+import time
+
+
+def timer(func):
+    """
+    Simple timer decorator.
+    """
+    def wrapper(*args, **kwargs):
+        start = time.perf_counter()
+        result = func(*args, **kwargs)
+        end = time.perf_counter()
+        print(f'Function duration: {(end - start):.2f} seconds')
+        return result
+
+    return wrapper
+
+
+@timer
+def example_multiplication_table(size: int) -> list:
+    """
+    Example of multiplication table.
+    """
+    time.sleep(1)
+    table = [
+        [i * j for j in range(1, size + 1)]
+        for i in range(1, size + 1)
+    ]
+    return table
+
+
+if __name__ == '__main__':
+    table_size: int = int(input())
+    example_multiplication_table(table_size)
