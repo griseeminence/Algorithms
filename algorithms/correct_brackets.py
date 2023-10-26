3385ed6b6fa031ebf60a5b3ec9571347a15bb6ef
griseeminence <p.kolt080@gmail.com>
2023-10-26 18:29:36 +0800

    add brackets

---
+++ algorithms/correct_brackets.py
@@ -0,0 +1,21 @@
+def correct_bracket(value: str) -> bool:
+    """
+    Check whether the bracket query is 'correct' or not.
+
+    :param value: str with the bracket query
+    :return: bool value if the bracket query is correct
+    """
+    stack: list = []
+    bracket_dict: dict = {')': '(', '}': '{', ']': '['}
+    for i in value:
+        if i in bracket_dict.values():
+            stack.append(i)
+        elif i in bracket_dict:
+            if not stack or stack.pop() != bracket_dict[i]:
+                return False
+    return not stack
+
+
+if __name__ == '__main__':
+    value = input()
+    print(correct_bracket(value))
