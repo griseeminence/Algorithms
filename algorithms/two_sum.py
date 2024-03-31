008dc7f61ef0e82d4db502e40325c2fef2e6c424
griseeminence <p.kolt080@gmail.com>
2024-03-31 12:48:40 +0200

    Algo find sum of two

---
+++ algorithms/two_sum.py
@@ -0,0 +1,13 @@
+def two_sum(nums: list, target: int):
+    """
+    This function iterates through the array once, so it has a time complexity of O(n),
+    where n is the number of elements in the array. The space complexity is also O(n)
+    due to the use of the dictionary to store the complements.
+    """
+    result_dict = {}
+    for i, y in enumerate(nums):
+        complement: int = target - y
+        if complement in result_dict:
+            return [result_dict[complement], i]
+        result_dict[y] = i
+    return []
