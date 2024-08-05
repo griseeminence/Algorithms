b872a9691a1a17ffc4eff518b1b084be3bcc17c0
griseeminence <p.kolt080@gmail.com>
2024-08-05 10:46:25 +0200

    Creating and filling a linked list

---
+++ Data Structures/Linked List.py
@@ -0,0 +1,35 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+        last = self.head
+        while last.next:
+            last = last.next
+        last.next = new_node
+
+    def print_list(self):
+        current = self.head
+        while current:
+            print(current.data, end=" -> ")
+            current = current.next
+        print("None")
+
+
+# Creating and filling a list
+llist = LinkedList()
+llist.append(1)
+llist.append(2)
+llist.append(3)
+
+llist.print_list()
