9f25992e67d5bcf42bd540c34566c8fd5c5f51c5
griseeminence <p.kolt080@gmail.com>
2024-08-06 14:24:45 +0200

    linear search

---
+++ search_algorithms/linear_search.py
@@ -0,0 +1,18 @@
+class LinearSearch:
+    def __init__(self, data):
+        self.data = data
+
+    def search(self, number):
+        for i in range(len(self.data)):
+            if self.data[i] == number:
+                return i
+        return -1
+
+# Example
+if __name__ == '__main__':
+    ls = LinearSearch([1, 2, 3, 4, 5, 6, 7, 8, 9])
+    result = ls.search(5)
+    print(result)
+
+    result = ls.search(10)
+    print(result)
