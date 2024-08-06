bc07b1b5305555e37f1fd57dfb227e8d0df81d87
griseeminence <p.kolt080@gmail.com>
2024-08-06 13:57:58 +0200

    recursions upd with staticmethod

--- data_structures/simple_recursions.py
+++ data_structures/simple_recursions.py
@@ -1,27 +1,48 @@
 class Factorials:
 
-    def __init__(self, data):
-        self.data = data
-
-    def factorial_rec(self, data=None):
-        if data is None:
-            data = self.data
+    @staticmethod
+    def factorial_recursive(data):
         if data == 0:
             return 1
         else:
-            return data * self.factorial_rec(data - 1)
+            return data * Factorials.factorial_recursive(data - 1)
 
-    def factorial_base(self, data=None):
-        if data is None:
-            data = self.data
+    @staticmethod
+    def factorial_iterative(data):
         result = 1
         for i in range(1, data + 1):
             result *= i
         return result
 
 
+class Fibonacci:
+
+    @staticmethod
+    def fib_recursive(data):
+        if data == 0:
+            return 0
+        elif data == 1:
+            return 1
+        else:
+            return Fibonacci.fib_recursive(data - 1) + Fibonacci.fib_recursive(data - 2)
+
+    @staticmethod
+    def fib_iterative(data):
+        if data == 0:
+            return 0
+        elif data == 1:
+            return 1
+        a, b = 0, 1
+        for _ in range(2, data + 1):
+            a, b = b, a + b
+        return b
+
+
 if __name__ == '__main__':
-    # Example
-    n = Factorials(5)
-    print("Factorial (recursive):", n.factorial_rec())
-    print("Factorial (iterative):", n.factorial_base())
+    # Examples
+    print('Factorials:')
+    print(Factorials.factorial_recursive(5))
+    print(Factorials.factorial_iterative(5))
+    print('Fibonacci:')
+    print(Fibonacci.fib_recursive(10))
+    print(Fibonacci.fib_iterative(10))
