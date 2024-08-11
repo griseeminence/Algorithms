bfe2a926fcf2fe0c60989ff964a31f64d49d7e0e
griseeminence <p.kolt080@gmail.com>
2024-08-11 07:23:06 +0200

    insert sort class

---
+++ sorting_algorithms/bubble_sort.py
@@ -0,0 +1,19 @@
+class BubbleSort:
+
+    @staticmethod
+    def sort(arr):
+        """Time complexity is O(n^2)"""
+        n = len(arr)
+        for i in range(n):
+            swapped = False
+            for j in range(0, n-i-1):
+                if arr[j]>arr[j+1]:
+                    arr[j], arr[j+1] = arr[j+1], arr[j]
+                    swapped = True
+            if not swapped:
+                break
+        return arr
+
+array = [64, 34, 25, 12, 22, 11, 90]
+sorted_array = BubbleSort.sort(array)
+print("Sorted array:", sorted_array)
---
+++ sorting_algorithms/insertion_sort.py
@@ -0,0 +1,21 @@
+class InsertionSort:
+
+    @staticmethod
+    def sort(arr):
+        """
+        Best case: O(n) - when the array is already sorted.
+        Average and worst cases: O(n^2) - when the array is sorted in reverse order.
+        """
+        for i in range(1, len(arr)):
+            key = arr[i]
+            j = i - 1
+            while j >= 0 and key < arr[j]:
+                arr[j + 1] = arr[j]
+                j -= 1
+            arr[j + 1] = key
+
+
+if __name__ == "__main__":
+    array = [12, 11, 13, 5, 6]
+    InsertionSort.sort(array)
+    print("Sorted array:", array)
