d93253a2074f306371aada3deaa604e6ebd651ed
griseeminence <p.kolt080@gmail.com>
2024-08-16 12:08:14 +0200

    longest common prefix leetcode

---
+++ leetcode/longest_common_prefix.py
@@ -0,0 +1,19 @@
+class Solution:
+
+    @staticmethod
+    def longest_common_prefix(lst: list[str]) -> str:
+        if not lst:
+            return ""
+        lst.sort()
+        i = 0
+        first, last = lst[0], lst[-1]
+        while first[i] == last[i]:
+            i += 1
+        return first[:i]
+
+
+if __name__ == "__main__":
+    # Example
+    example = ["flower", "flow", "flight"]
+    solution = Solution.longest_common_prefix(example)
+    print(solution)
--- leetcode/palindrome_number.py
+++ leetcode/palindrome_number.py
@@ -5,8 +5,9 @@ class Solution:
         return str(x) == str(x)[::-1]
 
 
-# Example
 if __name__ == '__main__':
     number = 121
     example = Solution.is_palindrome(number)
     print(f'If {number} is polindrome? - {example}')
+
+# Example
